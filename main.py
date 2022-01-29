309cb288bb6d89180cf5d9f6e81dac04071f340d
CreamBBQ <jgmd1306@gmail.com>
2022-01-29 12:29:54 -0300

    El juego funciona en su forma más básica

--- main.py
+++ main.py
@@ -1,9 +1,33 @@
+import random
+
 def read():
+    words = []
     with open("./archivos/palabras.txt", "r", encoding="utf-8") as f:
-        palabras = [word for word in f]
+        for word in f:
+            words.append(word.strip())
+    return words 
+
+def game():
+
+    words = read()
+    mainWord = words[random.randint(0, len(words))]
+    lettersMW = [i for i in mainWord]
+    hiddenLetters = ["_ " for i in mainWord]
+    print(*hiddenLetters)
+
+    for number in range(5): 
+        cont = 0
+        userLetter = input("Ingrese una letra: ")
+        userLetter = userLetter.lower()
+        for i in lettersMW:
+            if i == userLetter:
+                hiddenLetters[cont] = i
+            cont = cont + 1
+        print(*hiddenLetters)
 
 def run(): 
-    pass
+    game()
     
+
 if __name__ == "__main__":
     run()
