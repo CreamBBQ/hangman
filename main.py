d777027592c279c8a37198b679787051a4098a9f
CreamBBQ <jgmd1306@gmail.com>
2022-01-31 09:01:12 -0300

    Correción del sistema de vidas. Agrego mensajes de ganador y perdedor. Queda por solucionar el problema de las tildes

--- main.py
+++ main.py
@@ -11,19 +11,31 @@ def game():
 
     words = read()
     mainWord = words[random.randint(0, len(words))]
+    print(mainWord)
     lettersMW = [i for i in mainWord]
     hiddenLetters = ["_ " for i in mainWord]
     print(*hiddenLetters)
+    vidas = 5
 
-    for number in range(5): 
+    while vidas > 0:
+        win = False
         cont = 0
         userLetter = input("Ingrese una letra: ")
         userLetter = userLetter.lower()
         for i in lettersMW:
             if i == userLetter:
                 hiddenLetters[cont] = i
+                win = True
             cont = cont + 1
+        if win == False: 
+                vidas = vidas - 1
         print(*hiddenLetters)
+        if hiddenLetters == lettersMW: 
+            print("Felicidades! Ganaste")
+            break
+        if vidas == 0:
+            print("Perdiste. Vuelve a intentarlo")
+        
 
 def run(): 
     game()
