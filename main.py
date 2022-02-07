7e82cbe769030542ce76c47145375e2fed139da2
CreamBBQ <jgmd1306@gmail.com>
2022-02-07 08:54:14 -0300

    Organizo mejor el código. Pongo las letras en mayúscula

--- main.py
+++ main.py
@@ -12,56 +12,52 @@ def normalize(string):
         ("ú", "u"),
     )
     for x, y in repl:
-        string = string.replace(x, y)
+        string = string.replace(x, y).upper()
     return string
 
 
-def read():
+def getWord():
     words = []
     with open("./archivos/palabras.txt", "r", encoding="utf-8") as f:
         for word in f:
+            word = normalize(word)
             words.append(word.strip())
-    return words 
-
-
-def game():
-
-    words = read()
     mainWord = words[random.randint(0, len(words))]
-    mainWord = normalize(mainWord) #No lo pongo en read() porque pretendo hacer un nivel solo con tildes.
+    return mainWord
+
+
+def clear(): 
+    os.system("clear")
+
+
+def run():
+    mainWord = getWord()
     lettersMW = [i for i in mainWord]
     hiddenLetters = ["_ " for i in mainWord]
-    wrongWord = set()
+    wrongLetters = set()
+    attempts = 5
     print(*hiddenLetters)
-    vidas = 5
-
-    while vidas > 0:
+    
+    while attempts > 0:
         win = False
-        cont = 0
         userLetter = input("Ingrese una letra: ")
-        os.system("clear")
-        userLetter = userLetter.lower()
-        for i in lettersMW:
+        clear()
+        userLetter = userLetter.upper()
+        for count, i in enumerate(lettersMW):
             if i == userLetter:
-                hiddenLetters[cont] = i
+                hiddenLetters[count] = i
                 win = True
-            cont = cont + 1
         if win == False: 
-                vidas = vidas - 1
-                wrongWord.add("❌" + userLetter)
-                print(colored("Fallaste", "red") + ". vidas restantes: " + str(vidas) + "\n" + " ".join(wrongWord) + "\n")
+                attempts = attempts - 1
+                wrongLetters.add("❌" + userLetter)
+                print(colored("Fallaste", "red") + ". Vidas restantes: " + str(attempts) + "\n" + " ".join(wrongLetters) + "\n")
         else: 
-            print(colored("Acertaste", "green") + ". Te quedan " + str(vidas) + " vidas" + "\n" + " ".join(wrongWord) + "\n")
+            print(colored("Acertaste", "green") + ". Te quedan " + str(attempts) + " Vidas" + "\n" + " ".join(wrongLetters) + "\n")
         print(*hiddenLetters)
         if hiddenLetters == lettersMW: 
             print("\nFelicidades! Ganaste")
             break
-        if vidas == 0:
-            print("\nPerdiste, vuelve a intentarlo. La palabra correcta era: " + mainWord)
-        
-
-def run(): 
-    game()
+    print("\nPerdiste, vuelve a intentarlo. La palabra correcta era: " + mainWord)
 
 
 if __name__ == "__main__":
